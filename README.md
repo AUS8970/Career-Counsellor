60e8a568f670a0a39a0e7b4b397e9e7032c0e80b
aus8970 <www.aus8970@gmail.com>
2024-11-18 12:14:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Career Counseling Website
+
+You are here to develop a robust career counseling website that provides services to  users seeking career guidance. 
+For this project, will develop a Career Counseling Website designed to guide users on their career paths. The goal is to create a seamless, intuitive platform that offers personalized career advice, guidance, and resources to users seeking to make informed decisions about their professional futures. The website will be built using React, React Router, and Firebase for handling routing, user authentication, and data management.
+
+## Live Link
+[Career Counseling Website](https://ph-b10-assignment-9.netlify.com)
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXVmfPj2CviltI7xXnvRTafTeZ7UsVkAw",
+  authDomain: "career-counseling-websit-aus.firebaseapp.com",
+  projectId: "career-counseling-websit-aus",
+  storageBucket: "career-counseling-websit-aus.firebasestorage.app",
+  messagingSenderId: "186345694183",
+  appId: "1:186345694183:web:3d5772777169b9c06f5342"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
